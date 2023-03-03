c3c1ee7861b9ec1101905497fc94eada9126de61
Taylor Romero <me@taylorrome.ro>
2023-03-03 12:24:03 -0700

    minor: export postgres database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-//exports go here
+export { default as PostgresDatabase } from './PostgresDatabase'
+import { DatabaseFactory } from '@sprucelabs/data-stores'
+import PostgresDatabase from './PostgresDatabase'
+DatabaseFactory.addAdapter('postgres://', PostgresDatabase)
